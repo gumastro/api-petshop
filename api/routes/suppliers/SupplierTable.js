fa5668853aa9d15240f46b0ee98160512d455e18
Gustavo <gumastro@usp.br>
2021-03-17 21:41:41 -0300

    Chore: Create Supplier Model
    Create model to ease code maintenance

---
+++ api/routes/suppliers/SupplierTable.js
@@ -0,0 +1,22 @@
+const Model = require('./SupplierTableModel')
+
+module.exports = {
+    list() {
+        return Model.findAll()
+    },
+    add(supplier) {
+        return Model.create(supplier)
+    },
+    async findById(id) {
+        const found = await Model.findOne({
+            where: {
+                id: id
+            }
+        })
+        if (!found) {
+            throw new Error('Supplier not found!')
+        }
+
+        return found
+    }
+}
