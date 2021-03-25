089ebd3494d3c555a6c79b17aa7cf9f6ea36fd23
Gustavo <gumastro@usp.br>
2021-03-24 22:37:08 -0300

    Feat: Create Products GET route

--- api/routes/suppliers/products/index.js
+++ api/routes/suppliers/products/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router()
+const router = require('express').Router({ mergeParams: true })
+const Table = require('./ProductTable')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const products = await Table.list(req.params.idSupplier)
     res.send(
-        JSON.stringify([])
+        JSON.stringify(products)
     )
 })
 
